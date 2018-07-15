066e8e1076c08697456c8a677f4492197ebbf392
Dana Palmer <roiai4@hotmail.com>
2018-07-15 02:28:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
 
-
+#I'm genuinely not sure why this works and my own code doesn't.
+# def my_collect(programs)
+#  if programs.empty?
+#    "Empty array"
+# else
+#   counter = 0
+#   results = []
+#   while counter < 4
+#   my_collect(programs) do |lang|
+ #  results << yield(lang).upcase
+ #  counter += 1
+#end
+#results
+#end
+#end
